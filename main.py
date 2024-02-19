f9b07ec3d6d67f6156ba56ecd179e846bbe21359
koloideal <sabatt148@gmail.com>
2024-02-19 20:53:41 +0300

    first working version, added error handler for products with the same name, added check for duplicates

--- main.py
+++ main.py
@@ -1,35 +1,101 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import datetime
 
-url = f'https://www.kufar.by/l/mobilnye-telefony?ot=1&query=oneplus+nord+3&rgn=all&sort=lst.d&utm_filterOrigin=Search_suggester_3&utm_queryOrigin=Manually_typed&utm_suggestionType=Category_only'
-
+url = 'https://www.kufar.by/l/mobilnye-telefony/bez-posrednikov?ot=1&query=OnePlus+10+Pro&sort=lst.d'
 request = requests.get(url)
 soup = BeautifulSoup(request.text, 'html.parser')
 
 all_item = soup.find_all("a", class_="styles_wrapper__5FoK7")
-links = []
+photo_item = soup.find_all('img', class_="styles_image__ZPJzx")
+prices = soup.find_all('p', class_='styles_price__G3lbO')
+location = soup.find_all('div', class_='styles_secondary__MzdEb')
+
+about_item = []
 for link in all_item:
-    links.append(link['href'])
+
+    about_item.append([link['href']])
+
+for k, i in enumerate(prices):
+
+    about_item[k].append(i.text)
+
+for k, i in enumerate(location):
+
+    about_item[k].append(i.text)
+
 
 photo_links = []
-infini_dict = {}
-for i in enumerate(links):
-    new_request = requests.get(i[1])
-    new_soup = BeautifulSoup(new_request.text, 'html.parser')
-    h1_tag = new_soup.find('h1')
-    items = new_soup.find_all("img", class_="styles_slide__image__vertical__QdnkQ")
 
-    if items:
-        infini_dict.clear()
-        infini_dict[f'link{i[0]}'] = items[0]['src']
-        infini_dict[f'title{i[0]}'] = h1_tag.text
-        photo_links.append(infini_dict)
+for img in photo_item:
+
+    item = [img.get('src'), img.get('alt')]
+    photo_links.append(item)
+
+os.makedirs('content', exist_ok=True)    
+
+
+for k, photo_link in enumerate(photo_links):
+
+    alt = photo_link[1].replace(" ", "_").replace("/", "")
+    idx = 0
+
+    try:
+
+        os.makedirs(f'content/{alt}')
+
+    except FileExistsError:
+
+        with open(f'content/{alt}/about.txt', 'r', encoding='utf8') as e:
+            line = e.readline()
+
+        if line[18:53] == about_item[k][0][:35]:
+
+            continue
+
+        else:
+
+            try:
+
+                idx += 1
+
+                alt = photo_link[1].replace(" ", "_").replace("/", "") + '(' + str(idx) + ')'
+                os.makedirs(f'content/{alt}')
+
+                photo = requests.get(photo_link[0])
+
+                with open(f'content/{alt}/{alt}.jpg', 'wb') as out:
+                    out.write(photo.content)
+
+                with open(f'content/{alt}/about.txt', 'w', encoding='utf8') as about:
+                    about.write(f'Ссылка на товар : {about_item[k][0]}\n\n')
+                    about.write(f'Цена : {about_item[k][1]}\n\n')
+                    about.write(f'Локация товара : {about_item[k][2]}\n\n')
+
+            except FileExistsError as error:
+
+                with open('errors.txt', 'a', encoding='utf8') as errors:
+
+                    errors.write(f'Ошибка - {error}\n'
+                                 f'Дата - {datetime.datetime.now()}\n\n')
+
+                print('В текущей версии скрипта не поддерживается более 2-ух товаров с одинаковым названием,'
+                      'последний товар с одинаковым названием будет проигнорирован.')
+
+                continue
+
     else:
-        print(f"Нет изображения по этой ссылке {i[1]}")
 
-print(photo_links)
+        photo = requests.get(photo_link[0])
 
-os.makedirs('downloaded_images', exist_ok=True)
+        with open(f'content/{alt}/{alt}.jpg', 'wb') as out:
+            out.write(photo.content)
+
+        with open(f'content/{alt}/about.txt', 'w', encoding='utf8') as about:
+            about.write(f'Ссылка на товар : {about_item[k][0]}\n\n')
+            about.write(f'Цена : {about_item[k][1]}\n\n')
+            about.write(f'Локация товара : {about_item[k][2]}\n\n')
 
 
+print("Complete.")
